e33f42c4862c925c792c3332000c7af7f6d0589c
Felix-122140070 <felix.122140070@student.itera.ac.id>
2024-12-24 12:18:01 +0700

    Update login.php

--- views/login.php
+++ views/login.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Login - Toko Item Game</title>
+  <title>Login - TheSongsIsYours</title>
   <link rel="stylesheet" href="../public/css/style.css" />
   <script src="../public/js/login.js" defer></script>
 </head>
